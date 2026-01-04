baca2b2792492f7d7778a3c0fa03df237520ac15
Edgar Colmenero <colmeneroedgar15@gmail.com>
2026-01-04 00:36:29 -0600

    Add static export output (out folder)

---
+++ out/_next/static/aTanZzmJPljYbn6Nwm3dD/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-32b89cf805fbee27.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
